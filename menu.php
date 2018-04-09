b24ae81dbaf87db39d84d352d8edcb6ad3e5dbdc
Stefan Iancu <a1b2ff@yahoo.com>
2018-04-09 21:45:55 +0300

    Update menu.php

--- menu.php
+++ menu.php
@@ -18,7 +18,7 @@
               <li id="br_menu_noutati" <?php if ($currentURL == "index.php") echo "class=\"active\"";?> ><a href="index.php">Noutati</a></li>
               <li id="br_menu_rezultate" <?php if ($currentURL == "rezultate.php")  echo "class=\"active\"";?>><a href="rezultate.php">Rezultate</a></li>
               <li id="br_menu_resurse" <?php if ($currentURL == "resurse.php")  echo "class=\"active\"";?>><a href="resurse.php">Resurse</a></li>
-              <li id="br_menu_concurs" <?php if ($currentURL == "concurs.php")  echo "class=\"active\"";?>><a href="concurs.php">Concurs Iasi 2017</a></li>
+              <li id="br_menu_concurs" <?php if ($currentURL == "concurs.php")  echo "class=\"active\"";?>><a href="concurs.php">Turneu Emil Alexandrescu</a></li>
               <li class="dropdown">
                 <a data-toggle="dropdown" class="dropdown-toggle" href="#">Legaturi <span class="caret"></span></a>
                 <ul role="menu" class="dropdown-menu">
