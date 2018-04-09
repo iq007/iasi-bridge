856bd588a3a40a49fdd23a7960968d1cf6448f86
Stefan Iancu <a1b2ff@yahoo.com>
2018-04-09 21:52:57 +0300

    Update db.php

--- ext/db/db.php
+++ ext/db/db.php
@@ -2,7 +2,34 @@
 
 require_once('ext/db/medoo.min.php');
 
-echo getenv("DBNAME") ? getenv("DBNAME") : 'bridgeiasi';
+$connectstr_dbhost = '';
+$connectstr_dbname = '';
+$connectstr_dbusername = '';
+$connectstr_dbpassword = '';
+
+foreach ($_SERVER as $key => $value) {
+    if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
+        continue;
+    }
+    
+    $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+}
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define('DBNAME', $connectstr_dbname);
+
+/** MySQL database username */
+define('DBUSER', $connectstr_dbusername);
+
+/** MySQL database password */
+define('DBPASS', $connectstr_dbpassword);
+
+/** MySQL hostname : this contains the port number in this format host:port . Port is not 3306 when using this feature*/
+define('DBSERVER', $connectstr_dbhost);
 
 try{
     $database = new medoo([
